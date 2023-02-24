1748d1828aa4c3b63d2028a4cbc5db83ced237a5
Tippy <Pixie2shot@gmail.com>
2023-02-24 13:56:24 -0600

    Wizards News complete... oops

--- app.js
+++ app.js
@@ -1,9 +1,72 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
-const PORT = 1337;
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title} <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`
+  res.send(html);
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  res.send(`
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+     <div class="news-list">
+     <header><img src="/logo.png"/>Wizard News</header>
+      <p>
+       ${post.title}
+      <small>(by ${post.name})</small>
+      <p>
+    ${post.date}| ${post.content}
+     </div>
+    </body>
+  </html>`)
+});
+
+app.get('/post/:id', (req, res) => {
+  const id = req.params.id;
+  const post = find(id);
+  if(!post.id) {
+    throw new Error(`Looks like you've wandered too far into the woods`);
+  }
+});
+
+const PORT = process.env.PORT || 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
